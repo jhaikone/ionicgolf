28579b861c395b3bb3878c6b9680aca27485359e
Juuso <juusohaikonen@hotmail.com>
2016-07-04 13:11:06 +0300

    started tabs for achievements page

--- app/components/services/trophy-service/trophy-service.component.ts
+++ app/components/services/trophy-service/trophy-service.component.ts
@@ -2,9 +2,10 @@ import  { Injectable } from '@angular/core';
 
 const TROPHY = {
   ribbons: [
-    {name: 'no-result', label: 'Saavuta yli tripla bogey', icon: 'ion-ribbon-no-result', amount: 102},
-    {name: 'par', label: 'Saavuta par tulos', icon: 'ion-ribbon-par', amount: 0},
-    {name: 'birdie', label: 'Saavuta birdie tulos', icon: 'ion-ribbon-birdie', amount: 9}
+    {name: 'no-result', label: 'Merkkaa korttiin yli tripla bogey', icon: 'ion-ribbon-no-result', amount: 102, xp: 25},
+    {name: 'par', label: 'Merkkaa korttiin par tulos', icon: 'ion-ribbon-par', amount: 0, xp: 50},
+    {name: 'birdie', label: 'Merkkaa korttiin birdie tulos', icon: 'ion-ribbon-birdie', amount: 75},
+    // {name: 'eagle', label: 'Merkkaa korttiin eagle tulos', icon: 'ion-ribbon-birdie', amount: 150}
   ],
   medals: [
     {name: 'ten-rounds', label: 'Pelaa 10 kierrosta', icon: 'ion-no-result'},
